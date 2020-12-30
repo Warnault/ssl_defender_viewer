777a72f37291d4755a920bcba2e1f35055e15963
Lucile <ldauriac@etu.u-bordeaux.fr>
2020-12-30 17:16:52 +0100

    debut algo glouton

--- Solver.py
+++ Solver.py
@@ -192,33 +192,57 @@ class Solver :
 			#Relance l'algo avec un élément dans le tableau de plus
 			return self.algoExact(newTabDef, i+1)
 
-	def algoGlouton(self, tabKick, tabDef):
+
+	def algoGlouton(self, tabKick, tabDef, tabDefSol):
 		tabKickLength = len(tabKick)
 		tabDefLength = len(tabDef)
+		maxDefender = None
 		maxNeighbourLength = 0
-		if(tabKickLength):
-			self.solution = self.chercheDefenders(newTabDef)
+		if(tabKickLength == 0):
+			self.solution = self.chercheDefenders(tabDefSol)
 			return True
-		if(tabDefLength):
+		if(tabDefLength == 0):
 			return False
 		for defender in tabDef:
-			num = self.numKickOfDefender(tabKick, tabDef.get(defender)):
-			if(num > maxNeighbourLength)
+			num = self.numKickOfDefender(tabKick, tabDef.get(defender))
+			if(num > maxNeighbourLength):
 				maxNeighbourLength = num
-		
+				maxDefender = defender
+		tabDefSol.append(maxDefender)
+		tabKick = self.removeKickStop(maxDefender, tabKick)
+		tabDef.remove(maxDefender)
+		return self.algoGlouton(tabKick, tabDef, tabDefSol)
 	
 	#Récupérer les valeurs des defenders en fonctions de leurs nom
 	def chercheDefenders(self, tabDef):
-		return []
+		res = []
+		for defender in tabDef:
+			res.append(self.tabDefs.get(defender))
+		return res
 
 	#Verifie que le nombre de kicks bloquer est egale au total du nombre de kicks
 	def allKicksStop(self, tabDef):
+		#tableau kicks -> []
+		#Parcours def
+		#Si kick pas dans tableau alors add 
+		#si taille tableau kick == nombre de kick a arreté alors good
 		return True
 	
 	#Cherche le nombre de voisin en fonction de ceux de la list
-	def numKickOfDefender(self, tabKick, tabDef):
-		return 0
+	def numKickOfDefender(self, tabKick, defender):
+		cpt = 0
+		kickStopByDefender = self.tabDefs.get(defender)
+		for kickStop in kickStopByDefender : 
+			for kick in tabKick : 
+				if(kickStop == kick):
+					cpt = cpt + 1
+		return cpt
 
+	def removeKickStop(self, defender, tabKick):
+		kickRemove = self.tabDefs.get(defender)
+		for kick in kickRemove:
+			tabKick.remove(kick)
+		return tabKick
 
 def lies_in_range(interval,coord,radius):
 	values = [(coord-radius),coord,(coord+radius)]
