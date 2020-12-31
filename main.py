2a3012de3c7f043ef61b13637107532743904548
ARNAULT William <william.arnault@etu.u-bordeaux.fr>
2020-12-31 10:20:08 +0100

    correction main

--- main.py
+++ main.py
@@ -6,14 +6,18 @@ import json
 from board import *
 from Solver import *
 from algorithm.callOfAlgo import callOfAlgoExect, callOfAlgoGlouton
+ 
 
-if (len(sys.argv) < 4):
-  sys.exit("Usage: " + sys.argv[0] + "<version (1:prof /2:la notre)> <problem.json> <solution.json>")
+#if (len(sys.argv) < 4):
+#  sys.exit("Usage: " + sys.argv[0] + "<version (1:prof /2:la notre)> <problem.json> <solution.json>")
 
 version_path = sys.argv[1]
-algo_path = sys.argv[2]
-problem_path = sys.argv[3]
-solution_path = sys.argv[4]
+problem_path = sys.argv[2]
+solution_path = sys.argv[3]
+
+if ( len(sys.argv) >4):
+	algo_path = sys.argv[4]
+
 
 if (version_path == "1") :
 	with open(problem_path) as problem_file:
