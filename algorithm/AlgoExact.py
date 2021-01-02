0eb8921193552d18e90f0a57027eb569c98a98b8
ARNAULT William <william.arnault@etu.u-bordeaux.fr>
2021-01-02 17:56:46 +0100

    clean project

--- algorithm/AlgoExact.py
+++ algorithm/AlgoExact.py
@@ -22,21 +22,12 @@ class AlgoExact(AlgoSolver):
       list_permutation = itertools.permutations(self.list_defender,i)
       for ele in list_permutation:
         if(self.allKicksStop(ele)):
-          print("solver",ele)
           return self.chercheDefenders(ele)
     return []
-
-  def createListKickAndDef(self):
-    for key in self.dictNeighbors :
-      if "D" in key : 
-        self.list_defender.append(key)
-      else :
-        self.list_kick.append(key)
 
 	#Verifie que le nombre de kicks bloquer est egale au total du nombre de kicks
   def allKicksStop(self, tabDef):
     allKicksStop = self.list_kick.copy()
-    #print(allKicksStop)
     kickToStopByDefender = 0
     for defender in tabDef :
       kickToStopByDefender = self.dictNeighbors.get(defender)
@@ -46,21 +37,3 @@ class AlgoExact(AlgoSolver):
     if(len(allKicksStop) == 0):
       return True
     return False
-
-#  def algoExactRec(self, tabDef, index, list_defender):
-#    numDef = len(list_defender)
-#    if(index == numDef):
-#      return []
-#    for i in range(index):
-#      for d in list_defender:
-#        copy_tabDef = tabDef.copy()
-#        #copy_list_defender = list_defender.copy()
-#        if(d not in copy_tabDef):
-#          copy_tabDef.append(d)      
-#        #copy_list_defender.remove(d)
-#        if(self.allKicksStop(copy_tabDef)):
-#          print("Gagnee , normalement ",copy_tabDef)
-#          return copy_tabDef
-#    index+=1
-#    return self.algoExactRec,tabDef,index,list_defender)
-#
--- algorithm/callOfAlgo.py
+++ algorithm/callOfAlgo.py
@@ -5,8 +5,7 @@ from algorithm.AlgoGlouton import *
 
 def callOfAlgoExect(dictNeighbors,list_of_defencers):
   return AlgoExact(dictNeighbors,list_of_defencers)
-  print("Exect")
 
 def callOfAlgoGlouton(dictNeighbors,list_of_defencers):
   return AlgoGlouton(dictNeighbors,list_of_defencers)
-  print("Glouton")
+  
